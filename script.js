af0d9e8f5900daedacb4f8b55764fa1d8ed5d3be
ShaunK79 <wism79@gmail.com>
2025-04-28 19:51:19 +0100

    added updates on toggleSwitch, new functions. Added changes to Resistance, function changed added new variables. Add changes to calculate Current, new variables, new function

--- script.js
+++ script.js
@@ -6,28 +6,44 @@ let lightBulb = document.getElementById('lightBulb');
 
 // function
 function toggleSwitch() {
-    switchOn = !switchOn;
 
-    let switchButton = document.querySelector('.switch');
 
-    if (switchOn) {
-        switchButton.innerText = 'Switch ON';
+    let switchButton = document.getElementById('powerSwitch');
+    let lightBulb = document.getElementById('lightBulb');
+    let flowTop = document.getElementById("flowTop");
+    let flowBottom = document.getElementById("flowBottom");
+
+    if (switchButton.checked) {
         lightBulb.src = 'images/lightbulb-on.svg';
+        lightBulb.classList.add("glow");
+        flowTop.classList.add("flow-on");
+        flowBottom.classList.add("flow-on");
     } else {
-        switchButton.innerText = "Switch OFF";
         lightBulb.src = "images/lightbulb-off.svg";
+        lightBulb.classList.remove("glow");
+        flowTop.classList.remove("flow-on");
+        flowBottom.classList.remove("flow-on");
     }
     calculateCurrent();
 }
 
 function updateResistance() {
-    let slider = document.getElementById('resistorSlider');
-    resistance = slider.value;
-    document.getElementById('resistance').innerText = resistance;
+    const slider = document.getElementById('resistorSlider');
+    const lightBulb = document.getElementById('lightBulb');
+    const resistance = parseInt(slider.value);
+
+    if (document.getElementById('powerSwitch').checked) {
+        let glowStrength = 100 - resistance;
+        lightBulb.style.filter = `drop-shadow(0 0 ${glowStrength / 2}px yellow) drop-shadow(0 0 ${glowStrength}px orange)`;
+    }
     calculateCurrent();
 }
 
 function calculateCurrent() {
+    const voltage = 9;
+    const resistance = parseInt(document.getElementById('resistorSlider').value);
+    const switchOn = document.getElementById('powerSwitch').checked;
+
     let current = 0;
     if (switchOn) {
         current = (voltage / resistance).toFixed(2);
